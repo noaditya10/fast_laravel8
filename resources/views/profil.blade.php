f654d6b7993adaff9864ed2e6d94af5bcb39c97a
Novi Aditya <noaditya10@gmail.com>
2022-06-05 21:02:32 +0700

    blade profil

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profil Sekolah</title>
+</head>
+
+<body>
+    <h1>Profil sekolah CAMP404</h1>
+    <h4>Jl. Mampang Jakarta Selatan</h4>
+</body>
+
+</html>
