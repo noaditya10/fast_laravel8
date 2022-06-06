270a20745276e7a592c855da3b6e548a923b9a40
wachidrahman <wachidrahman@gmail.com>
2022-06-06 20:18:24 +0700

    Mengubah isi artikel pada halaman data-siswa

--- resources/views/pages/data-siswa.blade.php
+++ resources/views/pages/data-siswa.blade.php
@@ -7,35 +7,50 @@
     <title>Document</title>
 </head>
 <body>
-<table class="table table-striped">
+
+<h3>Data Siswa</h3>
+<table class="table table table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">No</th>
+      <th scope="col">Nama</th>
+      <th scope="col">NIS</th>
+      <th scope="col">Tanggal Lahir</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
+  <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>Johan Saputra</td>
+      <td>1011</td>
+      <td>2001-11-22</td>
     </tr>
     <tr>
       <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td>Deny Ferdiansyah</td>
+      <td>1012</td>
+      <td>2003-12-06</td>
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <td>Iqbal Ramadhan</td>
+      <td>1013</td>
+      <td>2003-06-06</td>
+    </tr>
+<tr>
+      <th scope="row">4</th>
+      <td>Rahmat Hidayat</td>
+      <td>1014</td>
+      <td>2003-05-14</td>
+    </tr>
+<tr>
+      <th scope="row">5</th>
+      <td>Azka Junio</td>
+      <td>1015</td>
+      <td>2003-09-04</td>
     </tr>
   </tbody>
 </table>
+
 </body>
 </html>
